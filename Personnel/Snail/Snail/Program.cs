5b067aafd5d8de12102e2eaf37ef9adf2d092c7a
JerryCleuet <150446786+JerryCleuet@users.noreply.github.com>
2024-08-30 11:43:46 +0200

    Snail + Parachute

--- Personnel/Snail/Snail/Program.cs
+++ Personnel/Snail/Snail/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
 
 namespace Snail
 {
@@ -10,30 +12,39 @@ namespace Snail
     {
         static void Main(string[] args)
         {
+            List<Snail> snails = new List<Snail>();
+            Snail toto = new Snail(10);
+            Snail titi = new Snail(15);
 
+            Console.CursorVisible = false;
 
-            string snail = "_@_ö";
-            int life = 50;
-            int position=0;
-
-            
-
-            do
+            for(int i = 0; i < 10; i++)
             {
+                snails.Add(new Snail(i));
+            }
+
+
+
+            while (snails[0].vie > 0) 
+            {
+                Console.Clear();
+                foreach (Snail snail in snails)
+                {
+                    Console.SetCursorPosition(snail.x, snail.y);
+                    Console.WriteLine(snail.alive);                 
+                    snail.move();                   
+                }
+                Thread.Sleep(50);             
+
+            }
+            foreach (Snail snail in snails)
+            {
+                Console.SetCursorPosition(snail.x, snail.y);
+                Console.WriteLine(snail.dead);
                
-                Console.SetCursorPosition(position,10);
-                Console.WriteLine(snail);
-                life--;
-                position++;
                 
-
-            } while (life > 0);
-
-            Console.SetCursorPosition(position, 10);
-            Console.WriteLine("____");
+            }
             Console.ReadLine();
-
-
 
 
         }
---
+++ Personnel/Snail/Snail/Snail.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snail
+{
+    internal class Snail
+    {
+        //attributs
+        public int vie = 50;          //points de vie
+        public int x = 0;
+        public int y;        //position
+        public string name;
+        
+        public string alive = "_@_ö";   //escargot vivant
+        public string dead = "____";    //escargot mort
+
+
+        //constructeur
+        public Snail(int y)
+        {
+          this.y = y;
+            this.name = "Escargot" + y;
+        }
+        public Snail()
+        {
+            y = 12;
+            this.name = "Escargot";
+
+        }
+
+
+
+
+        //méthode
+        public void move()
+        {
+            x++;
+            vie--;
+        }
+
+
+
+
+    }
+}
