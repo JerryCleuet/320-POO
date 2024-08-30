65ba98b18ef1359804d1593bfcf8ac35ac2e5c12
JerryCleuet <150446786+JerryCleuet@users.noreply.github.com>
2024-08-30 10:17:06 +0200

    Snail added

---
+++ Personnel/Snail/Snail/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snail
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string snail = "_@_ö";
+            int life = 50;
+            int position=0;
+
+            
+
+            do
+            {
+               
+                Console.SetCursorPosition(position,10);
+                Console.WriteLine(snail);
+                life--;
+                position++;
+                
+
+            } while (life > 0);
+
+            Console.SetCursorPosition(position, 10);
+            Console.WriteLine("____");
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
